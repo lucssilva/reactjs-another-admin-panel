415ec2191ab91701a3035d0a65a309613573facc
Lucas Silva <lucssbarrosdasilva@gmail.com>
2021-07-11 10:46:10 -0300

    feat: Using a repository to fetch user data from mock server in UserScreen

---
+++ src/api/userRepository.js
@@ -0,0 +1,42 @@
+import api from '../services/httpClient';
+import { USERS_ENDPOINT } from '../helpers/apiConsts';
+
+export const retrieveUsers = async () => {
+    try {
+        const response = await api.get(USERS_ENDPOINT);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
+
+export const fetchUser = async (userId) => {
+    try {
+        const response = await api.get(`${USERS_ENDPOINT}/${userId}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return {};
+    }
+}
+
+export const updateUser = async (user) => {
+    try {
+        const response = await api.put(`${USERS_ENDPOINT}/${user.id}`, user);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return {};
+    }
+};
+
+export const deleteUser = async (userId) => {
+    try {
+        await api.delete(`${USERS_ENDPOINT}/${userId}`);
+        return userId;
+    } catch (error) {
+        console.log(error);
+        return 0;
+    }
+};
--- src/view/screens/List/UsersListScreen.jsx
+++ src/view/screens/List/UsersListScreen.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { UsersListWidget } from "../../widgets";
+import * as repo from "../../../api/userRepository";
 
 const UsersListScreen = () => {
-  const users = [{
-    "name": "Lucas Silva",
-    "email": "lucssbarrosdasilva@gmail.com"
-  }];
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const _users = await repo.retrieveUsers();
+      if (_users) setUsers(_users);
+    };
+
+    getUsers();
+  }, []);
 
   return <UsersListWidget users={users} />
 }
