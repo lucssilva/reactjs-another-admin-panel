c3e0777b5d31221bc08cae266911fff83716ab65
Lucas Silva <lucssbarrosdasilva@gmail.com>
2021-07-12 17:59:53 -0300

    fix: Updating UserModalWidget to create user and dispose modal

--- src/view/widgets/UserModal/UserModal.jsx
+++ src/view/widgets/UserModal/UserModal.jsx
@@ -4,17 +4,23 @@ import { PlusIcon } from "../../elements";
 export const UserModalWidget = ({
     createHandler = () => { },
 }) => {
-    const [user, setUser] = useState({ });
+    const MODAL_ID = "userModal";
+    const [user, setUser] = useState({});
     const save = (e) => {
-        // e.preventDefault();
+        e.preventDefault();
         if (user.name === "" || user.email === "") return;
-        createHandler(user)
+        createHandler(user);
+        if (e.target === document.getElementById(MODAL_ID)) {
+            setUser({});
+            const close = document.getElementById("closeModal");
+            close.click();
+        }
     }
 
     return (
         <Fragment>
-            <button type="button" class="btn btn-lg btn-primary shadow"
-                data-bs-toggle="modal" data-bs-target="#userModal"
+            <button type="button" className="btn btn-lg btn-primary shadow"
+                data-bs-toggle="modal" data-bs-target={`#${MODAL_ID}`}
                 style={{
                     position: "fixed",
                     bottom: "40px",
@@ -25,39 +31,39 @@ export const UserModalWidget = ({
                     style={{ verticalAlign: "baseline" }} />
             </button>
 
-            <form class="modal fade" id="userModal" onSubmit={save}
-                tabindex="-1" aria-labelledby="userModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="userModalLabel">Title</h5>
+            <form className="modal fade" id={MODAL_ID} onSubmit={save}
+                tabIndex="-1" aria-labelledby="userModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="userModalLabel">Title</h5>
 
-                            <button type="button" class="btn-close"
+                            <button id="closeModal" type="button" className="btn-close"
                                 data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
-                            <div class="mb-3">
-                                <label for="userName" class="form-label">Full name</label>
+                        <div className="modal-body">
+                            <div className="mb-3">
+                                <label htmlFor="userName" className="form-label">Full name</label>
                                 <input type="text"
                                     id="userName"
-                                    class="form-control"
+                                    className="form-control"
                                     value={user.name}
-                                    onChange={(e) => setUser({ ...user, name:e.target.value })}
+                                    onChange={(e) => setUser({ ...user, name: e.target.value })}
                                     required />
                             </div>
-                            <div class="mb-3">
-                                <label for="userEmail" class="form-label">Email address</label>
+                            <div className="mb-3">
+                                <label htmlFor="userEmail" className="form-label">Email address</label>
                                 <input type="email"
                                     id="userEmail"
-                                    class="form-control"
+                                    className="form-control"
                                     value={user.email}
-                                    onChange={(e) => setUser({ ...user, email:e.target.value })}
+                                    onChange={(e) => setUser({ ...user, email: e.target.value })}
                                     required />
                             </div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" className="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
